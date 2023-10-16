3889898dbd32d5201c7e86645a52579bede946cc
TomatitoFeliz <odassorodztomas@gmail.com>
2023-10-16 12:15:48 +0100

    1.0.2
    Máquina expendedora añadida y texturizada

--- MiniGameHeladería/Assets/MenúPrincipal/MenúPincipalGameManager.cs
+++ MiniGameHeladería/Assets/MenúPrincipal/MenúPincipalGameManager.cs
@@ -2,15 +2,32 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class MenúPincipalGameManager : MonoBehaviour
 {
     [SerializeField]
-    GameObject tutorialCanvas, sonidoCanvas;
+    GameObject tutorialCanvas, sonidoCanvas, recuadroMenu;
 
-
+    private void Start()
+    {
+        recuadroMenu.SetActive(false);
+    }
     public void StarLevels()
     {
         SceneManager.LoadScene("Game01");
+    }
+    public void Ajustes()
+    {
+        recuadroMenu.SetActive(true);
+        if (recuadroMenu != null)
+        {
+            LeanTween.moveLocalX(recuadroMenu, -520f, 1.5f).setEaseInBounce();
+        }
+    }
+    public void SalirMenu()
+    {
+        LeanTween.moveLocalX(recuadroMenu, -882, 1.5f).setEaseInBounce().setOnComplete(Start);
+        
     }
     public void Sonido()
     {
